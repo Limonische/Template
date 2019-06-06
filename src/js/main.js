624a49f150d722de1b2784f13eb2d503bd1fcfd3
Limonische <gulisnikita@gmail.com>
2019-06-07 01:34:26 +0300

    Add default onLoad function Call lazyLoadImages and objectFit from onLoad

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,12 @@ import '../sass/styles.sass';
 // Libraries
 
 // Modules
+import lazyLoadImages from './modules/lazyLoadImages';
+import objectFit from './modules/objectFit';
+
+const onLoad = () => {
+    lazyLoadImages();
+    objectFit();
+};
+
+window.addEventListener('load', onLoad);
