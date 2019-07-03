1f202e7a5ce06cbe1ba8022d4b644f72f1447f43
Limonische <gulisnikita@gmail.com>
2019-07-04 01:17:43 +0300

    Remove temporary fix for eslint-loader

--- webpack/js.js
+++ webpack/js.js
@@ -1,22 +1,12 @@
 // JavaScript module
 
-const stylish = require('eslint/lib/cli-engine/formatters/stylish');
-
 module.exports = {
     module: {
         rules: [
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                use: [
-                    'babel-loader',
-                    {
-                        loader: 'eslint-loader',
-                        options: {
-                            formatter: stylish,
-                        },
-                    },
-                ],
+                use: ['babel-loader', 'eslint-loader'],
             },
         ],
     },
