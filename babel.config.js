604030e72cc0743b5680c2ab82a9e40ed3e3f368
Limonische <gulisnikita@gmail.com>
2020-02-15 19:45:29 +0300

    Update dependencies; Remove nullish coalescing operator plugin; Remove optional chaining plugin;

--- babel.config.js
+++ babel.config.js
@@ -13,12 +13,7 @@ module.exports = ({ cache }) => {
             },
         ],
     ];
-    const plugins = [
-        '@babel/plugin-proposal-class-properties',
-        '@babel/plugin-proposal-private-methods',
-        '@babel/plugin-proposal-optional-chaining',
-        '@babel/plugin-proposal-nullish-coalescing-operator',
-    ];
+    const plugins = ['@babel/plugin-proposal-class-properties', '@babel/plugin-proposal-private-methods'];
 
     return {
         presets,
